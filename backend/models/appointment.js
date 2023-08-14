65db5fe7620b856bd58ce5a5084a3841c7afb8b7
Zeeshan <zeeshanibrahim78@gmail.com>
2023-08-15 01:11:57 +0500

    no

--- backend/models/appointment.js
+++ backend/models/appointment.js
@@ -1,26 +1,20 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Appointment extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
+    static associate(models) {}
   }
-  Appointment.init({
-    appointment_datetime: DataTypes.DATE,
-    appointment_type: DataTypes.STRING,
-    duration: DataTypes.INTEGER,
-    deleted: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'Appointment',
-  });
+  Appointment.init(
+    {
+      appointment_datetime: DataTypes.DATE,
+      appointment_type: DataTypes.STRING,
+      duration: DataTypes.INTEGER,
+      deleted: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Appointment",
+    }
+  );
   return Appointment;
 };
--- backend/models/firm.js
+++ backend/models/firm.js
@@ -1,26 +1,20 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Firm extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
+    static associate(models) {}
   }
-  Firm.init({
-    name: DataTypes.STRING,
-    city: DataTypes.STRING,
-    state: DataTypes.STRING,
-    zip: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Firm',
-  });
+  Firm.init(
+    {
+      name: DataTypes.STRING,
+      city: DataTypes.STRING,
+      state: DataTypes.STRING,
+      zip: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Firm",
+    }
+  );
   return Firm;
 };
